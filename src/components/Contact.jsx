cc0abe958c6d7db348ec25c1fcc6fc797d5a0860
rohitpatmase026-create <rohitpatmase026@gmail.com>
2026-09-05 14:00:06 +0530

    Fix phone number HTML structure in Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -55,7 +55,7 @@ function Contact() {
               <div>
                 <h3 className="font-bold text-xl">Phone</h3>
                 <p className="text-gray-600">
-                  +91 9021843831
+                 <p>+91 9021843831</p>
                 </p>
               </div>
             </div>
